b4f6e74f5b2ea7b783c9f442882e47758290d97d
Tom Silver <tomssilver@gmail.com>
2024-09-02 15:14:45 -0400

    add test for random approach

---
+++ tests/approaches/test_random_approach.py
@@ -0,0 +1,22 @@
+"""Tests for random_approach.py."""
+
+import numpy as np
+
+from python_research_starter.approaches.random_approach import RandomApproach
+from python_research_starter.benchmarks.maze_benchmark import MazeBenchmark
+
+
+def test_random_approach():
+    """Tests for RandomApproach()."""
+    benchmark = MazeBenchmark()
+    approach = RandomApproach(
+        benchmark.get_actions(),
+        benchmark.get_next_state,
+        benchmark.get_cost,
+        benchmark.check_goal,
+    )
+    assert approach.get_name() == "random"
+    rng = np.random.default_rng(123)
+    task = benchmark.generate_tasks(1, "train", rng)[0]
+    plan = approach.generate_plan(task, "test", 1e-1, rng)
+    assert len(plan) > 0
